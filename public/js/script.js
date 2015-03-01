efb4f3a2282f7e2e2b101046787468931963ffc2
Ben Gundersen <ben@bengundersen.com>
2015-03-01 11:55:46 -0500

    Getting started with React

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,54 @@
 'use strict';
 
+var UserList = React.createClass({
+  getInitialState: function() {
+    return {
+      data: []
+    };
+  },
+  componentDidMount: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        console.log(data);
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  render: function() {
+    var userNodes = function() {
+      this.props.data.map(function(user) {
+        return (
+          <Comment>
+            {this.props.name}
+          </Comment>
+        );
+      });
+    };
+    return (
+      <ul className="user-list">
+        <h1>Users</h1>
+        {userNodes}
+      </ul>
+    );
+  }
+});
+
+var User = React.createClass({
+  render: function() {
+    return (
+      <li className="user">
+        {this.props.name}
+      </li>
+    );
+  }
+});
+
+React.render(
+  <UserList url="/data" />,
+  document.getElementById('container')
+);
