c0a9f096070bf39cdcc5d1bfee174627296539ab
Ben Gundersen <ben@bengundersen.com>
2015-02-28 21:03:48 -0500

    test request

--- server.js
+++ server.js
@@ -6,7 +6,20 @@ var _ = require('lodash');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello, committer');
+  var options = {
+    url: 'https://api.github.com/users/bgun/repos',
+    headers: {
+      'User-Agent': 'bgun'
+    }
+  };
+  request.get(options, function(error, resp, body) {
+    console.log(body);
+    if (!error && resp.statusCode == 200) {
+      res.send(JSON.parse(body)[0]);
+    } else {
+      res.send("error");
+    }
+  });
 });
 
 app.listen(9005);
