3388f7dea0b19dcc5b39ccbe47cad743b813a333
Ben Gundersen <ben@bengundersen.com>
2015-03-06 02:27:37 -0500

    Caching using node-dirty instead of localstorage

--- public/js/script.js
+++ public/js/script.js
@@ -8,48 +8,41 @@ var UserList = React.createClass({
       data: []
     };
   },
-  loadComments: function() {
+  loadUsers: function() {
     var time = new Date().getTime();
     // Avoid crushing GitHub rate limit (5000 per hour).
     // The more users you're checking in on, the longer this should be.
     var delay_seconds = 300;
-    var msg, cached_data, time_left;
-    cached_data = JSON.parse(localStorage.getItem('users'));
+    var msg;
+    mixpanel.track("Loading fresh data");
     $container.addClass('loading');
-    if(cached_data && (cached_data.updated+(delay_seconds*1000) > time)) {
-      time_left = Math.round(delay_seconds - ((time-cached_data.updated)/1000));
-      msg = "Showing recently cached data. " + time_left + " seconds till delay expires";
-      mixpanel.track("Showing cached data");
-      $container.removeClass('loading');
-      this.setState({
-        message: msg,
-        data: cached_data.users
-      });
-    } else {
-      console.log("No recently cached data found. Loading...");
-      mixpanel.track("Loading fresh data");
-      $.ajax({
-        url: this.props.url,
-        dataType: 'json',
-        success: function(data) {
-          $container.removeClass('loading');
-          localStorage.setItem('users', JSON.stringify(data));
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        var msg;
+        $container.removeClass('loading');
+        if(data.used_cache) {
+          msg = "Showing recently cached data. " + data.cache_expires + " seconds left";
+          mixpanel.track("Showing cached data");
+        } else {
+          msg = "Fetching new data";
           mixpanel.track("Loaded data successfully");
-          this.setState({
-            message: "Fetching new data",
-            data: data.users
-          });
-        }.bind(this),
-        error: function(xhr, status, err) {
-          $container.removeClass('loading');
-          mixpanel.track("Error loading data");
-          console.error(this.props.url, status, err.toString());
-        }.bind(this)
-      });
-    }
+        }
+        this.setState({
+          message: msg,
+          data: data.users
+        });
+      }.bind(this),
+      error: function(xhr, status, err) {
+        $container.removeClass('loading');
+        mixpanel.track("Error loading data");
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
   },
   componentDidMount: function() {
-    this.loadComments();
+    this.loadUsers();
   },
   render: function() {
     var userNodes = this.state.data.map(function(user) {
--- server.js
+++ server.js
@@ -2,17 +2,23 @@
 
 var express = require('express');
 var request = require('request');
-var Q = require('q');
-var _ = require('lodash');
-var moment = require('moment-timezone');
-var port = process.env.PORT || 9005;
+var Q       = require('q');
+var _       = require('lodash');
+var dirty   = require('dirty');
+var moment  = require('moment-timezone');
 
+// Time zone for calculating "X hours ago"
 moment().tz("America/New_York").format();
 
-var app = express();
+var port = process.env.PORT || 9005;
+var app  = express();
+var db   = dirty('dirty.db');
+
+// pass through public directory
 app.use('/public', express.static(__dirname + "/public"));
 
-var usernames = (process.env.GITHUB_USERNAMES || '').split(',');
+var usernames = (process.env.GITHUB_USERNAMES || 'bgun,github').split(',');
+var cache_delay = 300; // seconds before recache
 
 // replacement values
 var tokens = {
@@ -30,16 +36,16 @@ var formatString = function(str, tokens) {
   return str;
 };
 
-var authParams = '?'+[
-  'client_id={{client_id}}',
-  'client_secret={{client_secret}}'
-].join('&');
-
 // GitHub endpoints
 var endpoints = {
   user : '{{gh_base}}users/{{username}}',
   repos: '{{gh_base}}users/{{username}}/repos'
 };
+
+var authParams = '?'+[
+  'client_id={{client_id}}',
+  'client_secret={{client_secret}}'
+].join('&');
 
 var makeRequest = function(type, username) {
   var tk = _.extend({
@@ -51,17 +57,19 @@ var makeRequest = function(type, username) {
       'User-Agent': 'bgun'
     }
   };
+  console.log(options.url);
   return options;
 };
 
 var getUser = function(username) {
+  console.log("Getting user",username);
   var deferred = Q.defer();
   request.get(makeRequest('user', username), function(error, resp, body) {
     if (!error && resp.statusCode == 200) {
       var data = JSON.parse(body);
       deferred.resolve(data);
     } else {
-      deferred.reject('failed');
+      deferred.reject('Failed to get user: '+username);
     }
   });
   return deferred.promise;
@@ -88,75 +96,122 @@ var getLastUpdatedRepoForUser = function(username) {
   return deferred.promise;
 };
 
-var getLiveData = function() {
+var getCachedData = function() {
+
+    db.set('john', {eyes: 'blue'});
+    console.log('Added john, he has %s eyes.', db.get('john').eyes);
+
+    db.set('bob', {eyes: 'brown'}, function() {
+      console.log('User bob is now saved on disk.')
+    });
+
+    db.forEach(function(key, val) {
+      console.log('Found key: %s, val: %j', key, val);
+    });
+};
+
+var getData = function(expired, now) {
   var deferred = Q.defer();
-  var user_promises = _.map(usernames, function(n) {
-    return getUser(n);
-  });
-  var repo_promises = _.map(usernames, function(n) {
-    return getLastUpdatedRepoForUser(n);
-  });
-  // fire all the things!
-  Q.all(user_promises)
-    .then(function(users) {
+  var user_promises;
+  var repo_promises;
 
-      Q.all(repo_promises)
-        .then(function(repos) {
+  if(!expired) {
+    console.log("Loading from cache");
+    deferred.resolve(db.get('users'));
+  } else {
+    console.log("Loading from live");
+    user_promises = _.map(usernames, function(n) {
+      return getUser(n);
+    });
+    repo_promises = _.map(usernames, function(n) {
+      return getLastUpdatedRepoForUser(n);
+    });
+    // fire all the things!
+    Q.all(user_promises)
+      .then(function(users) {
 
-          var output = _(repos)
-            .map(function(r, i) {
-              return _.extend({
-                username: usernames[i],
-                pushed_at   : r.pushed_at ? r.pushed_at : '',
-                last_pushed : r.pushed_at ? moment(r.pushed_at).calendar() : 'Never'
-              }, {
-                avatar   : users[i].avatar_url,
-                name     : users[i].name || 'unknown',
-                home_url : users[i].html_url
-              });
-            })
-            .sortBy('pushed_at')
-            .reverse()
-            .value();
+        Q.all(repo_promises)
+          .then(function(repos) {
 
-          console.log("Returning live data, "+output.length+" users");
-          deferred.resolve({
-            updated: new Date().getTime(),
-            users: output
-          });
+            var output = _(repos)
+              .map(function(r, i) {
+                return _.extend({
+                  username: usernames[i],
+                  pushed_at   : r.pushed_at ? r.pushed_at : '',
+                  last_pushed : r.pushed_at ? moment(r.pushed_at).calendar() : 'Never'
+                }, {
+                  avatar   : users[i].avatar_url,
+                  name     : users[i].name || 'unknown',
+                  home_url : users[i].html_url
+                });
+              })
+              .sortBy('pushed_at')
+              .reverse()
+              .value();
 
-        })
-        .done();
-    })
-    .done();
+            db.set('updated', now);
+            db.set('users',   output);
+
+            console.log("Returning live data, "+output.length+" users");
+
+            deferred.resolve(output);
+          })
+          .catch(function(msg) {
+            throw new Error("Error in repo:",msg);
+          })
+          .done();
+      })
+      .catch(function(msg) {
+        throw new Error("Error in users:",msg);
+      })
+      .done();
+  }
 
   return deferred.promise;
 };
 
-var getTestData = function() {
-  var deferred = Q.defer();
-  console.log("Returning test data");
-  deferred.resolve({
-    test: true,
-    updated: 0,
-    users: test_data.users
-  });
-  return deferred.promise;
-};
+
+
+// Express routes
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname+'/public/html/index.html');
 });
 
 app.get('/data', function(req, res) {
-  console.log(req.query);
-  var promise = req.query.test ? getTestData() : getLiveData();
-  promise
+  var now = new Date().getTime();
+  var updated = db.get('updated') || 0;
+  var since_cache = (now - updated) / 1000;
+  var cache_expires = cache_delay - since_cache;
+  var force = req.query.force ? true : false;
+  var expired = force || cache_expires < 0;
+
+  getData(expired, now)
     .then(function(data) {
-      res.send(data);
+      var end = new Date().getTime();
+      res.send({
+        updated       : updated,
+        elapsed       : end - now,     // how long did the request take?
+        cache_expires : cache_expires, // seconds to expire
+        used_cache    : expired,
+        users         : data
+      });
+    })
+    .catch(function(msg) {
+      console.log("Error in route:",msg);
     })
     .done();
 });
 
-console.log("Starting server on port "+port);
-app.listen(port);
+
+
+// initialize node-dirty
+
+console.log("Initializing database");
+db.on('load', function() {
+  console.log("Starting server on port "+port);
+  app.listen(port);
+});
+db.on('drain', function() {
+  console.log("All records written to disk");
+});
